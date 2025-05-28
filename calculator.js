f549f41691572108cfdd9b71797e8e3f3b36725a
Shubham Sharma <shubham@Shubhams-MacBook-Air.local>
2025-05-28 23:05:58 +0530

    Feat: Added empty string, one and two number test.

--- calculator.js
+++ calculator.js
@@ -1,5 +1,8 @@
-function add(number) {
-  return 0;
+function add(numbers) {
+  if (numbers === "") return 0;
+  const nums = numbers.split(",").map(Number);
+  return nums.reduce((sum, n) => sum + n, 0);
 }
+
 
 module.exports = add;
--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,19 @@
 const add = require("./calculator");
 
+// Test for empty string.
 test("return 0 for empty string", () => {
   expect(add("")).toBe(0);
 });
+
+
+// Test for one number.
+test('returns the number when one number is given', () => {
+  expect(add("5")).toBe(5);
+});
+
+// Test for two number.
+test('returns the sum of two numbers', () => {
+  expect(add("1,2")).toBe(3);
+});
+
+
